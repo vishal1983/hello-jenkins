9b9a4a761ccfe5d50c5173b3c3a92dfa1df72f06
vishal1983 <connectvishalreddy@gmail.com>
2015-07-30 00:19:13 -0600

    changed hmepage text to hello world2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world1');
+  res.send('hello world2');
 });
  
 app.listen(process.env.PORT || 5000);
